9f81eed6b05a603b66304c0842bdc4b83f0b59e1
rsteddd <vladislavgucci~@gmail.com>
2024-01-29 18:37:01 +0200

    fist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link} from "react-router-dom";
+import Counter from "./components/Counter/Counter";
+import Layout from "./Layout";
+import Modal from "./components/Modal/Modal";
+import Quiz from "./components/Quiz/Quiz";
+import styles from "./Layout.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+    <Routes>
+       <Route path="/" element={<Layout />}>
+        <Route path="counter" element={<Counter />}/>
+        <Route path="modal" element={<Modal />}/>
+        <Route path="quiz" element={<Quiz />}/>
+        </Route>
+    </Routes>
+    </>
+  )
 }
 
 export default App;
